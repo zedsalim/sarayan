b74ceefbb60e0ed82fb8ecfb9e7ce441a46b1b53
Salim Zaidi <salimzaiidi@gmail.com>
2026-02-23 09:08:04 +0100

    build: add purgecss config and purge bootstrap/icons to reduce CSS bundle size

---
+++ purgecss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  css: ['assets/css/bootstrap.rtl.min.css'],
+  content: ['index.html', 'assets/js/script.js'],
+  output: 'assets/css/bootstrap.rtl.min.purged.css',
+  safelist: {
+    standard: [
+      'show',
+      'collapsing',
+      'offcanvas',
+      'offcanvas-start',
+      'offcanvas-end',
+      'modal',
+      'modal-backdrop',
+      'dropdown-menu',
+      'fade',
+    ],
+    greedy: [/^(show|collaps|offcanvas|modal|dropdown|active|disabled|rtl)/],
+  },
+};
